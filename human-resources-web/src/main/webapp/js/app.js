045183f44b7e56f23bc2e482375a1c517a45152a
Nelson Sandoval <techednelson@gmail.com>
2018-08-10 00:13:47 +0300

    fix: refactor inserted data from liquibase files

---
+++ human-resources-web/src/main/webapp/js/app.js
@@ -0,0 +1,56 @@
+// main module called myApp
+angular.module('myApp', [
+  'ngRoute', 
+  'employeeCtrl',
+  'employeeService',
+  'departmentCtrl', 
+  'departmentService',
+  'addressService'
+])
+
+// configure routes
+  .config(function($routeProvider) {
+    $routeProvider
+    
+    .when('/', { 
+      templateUrl : 'views/home.html',
+      controller : ''
+    })
+
+    .when('/new_employee', {
+      templateUrl : 'views/create_employee.html',
+      controller : 'createEmployee'
+    })
+
+    .when('/edit_employee/:id', {
+      templateUrl : 'views/edit_employee.html',
+      controller : 'updateEmployee'
+    })
+
+    .when('/show_employees', {
+      templateUrl : 'views/show_employees.html',
+      controller : 'employees'
+    })
+
+    .when('/new_department', {
+      templateUrl : 'views/create_department.html',
+      controller : 'createDepartment'
+    })
+
+    .when('/edit_department/:id', {
+      templateUrl : 'views/edit_department.html',
+      controller : 'updateDepartment'
+    })
+
+    .when('/show_departments', {
+      templateUrl : 'views/show_departments.html',
+      controller : 'departments'
+    })
+
+    .otherwise({
+      redirectTo: 'views/home.html/'
+    });
+
+});
+
+
